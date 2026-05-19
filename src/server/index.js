973c794f2d5d2f43d40699877c595f4841c07d20
Bagus Eka Pramudya <bepramudyaa@gmail.com>
2026-05-19 17:51:38 +0700

    refactor(routes): add authenticate middleware and cors middleware

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 import routes from '../routes/index.js';
 import ErrorHandler from '../middlewares/error.js';
 
 const app = express();
+
+app.use(
+  cors({
+    origin: '*',
+  }),
+);
 
 app.use(express.json());
 app.use(routes);
