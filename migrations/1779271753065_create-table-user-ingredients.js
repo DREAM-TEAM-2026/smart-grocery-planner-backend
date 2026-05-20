9d81f72df112a817a928bbb4b83140202814f68d
Bagus Eka Pramudya <bepramudyaa@gmail.com>
2026-05-20 19:22:59 +0700

    feat(migrations): add many migrations for Main Feature

---
+++ migrations/1779271753065_create-table-user-ingredients.js
@@ -0,0 +1,59 @@
+/* eslint-disable camelcase */
+
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+// export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.createTable('ingredients', {
+    id: { type: 'UUID', primaryKey: true },
+    name: { type: 'VARCHAR(255)', notNull: true, unique: true }, // Nama bahan unik!
+    category: { type: 'ingredient_category', notNull: true },
+  });
+
+  pgm.createTable('user_ingredients', {
+    id: {
+      type: 'UUID',
+      primaryKey: true,
+    },
+    user_id: {
+      type: 'UUID',
+      references: '"neon_auth"."user"',
+      onDelete: 'CASCADE',
+      notNull: true,
+    },
+    ingredient_id: {
+      type: 'UUID',
+      references: '"public"."ingredients"',
+      onDelete: 'CASCADE',
+      notNull: true,
+    },
+    quantity: {
+      type: 'SMALLINT',
+      notNull: true,
+      default: 0,
+    },
+  });
+
+  pgm.createIndex('user_ingredients', 'user_id');
+
+  pgm.addConstraint('user_ingredients', 'unique_user_ingredient', {
+    unique: ['user_id', 'ingredient_id'],
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropTable('user_ingredients', { cascade: true });
+  pgm.dropTable('ingredients', { cascade: true });
+};
