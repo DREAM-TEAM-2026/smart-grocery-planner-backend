d3fc0e26963590c84e55fdb5e33db214e9d5ef11
Bagus Eka Pramudya <bepramudyaa@gmail.com>
2026-05-20 08:42:51 +0700

    chore: removing unused comments and code

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -10,14 +10,9 @@ const authenticate = async (req, res, next) => {
       });
     }
 
-    // Ambil string tokennya saja
     const token = authHeader.split(' ')[1];
-
-    // Validasi token via TokenManager
     const authData = await TokenManager.verifyNeonToken(token);
 
-    // Simpan objek user ke request Express agar bisa dipakai di controller
-    // (Misal: req.user.id untuk dipakai sebagai `user_id` di Notes)
     req.user = authData.user;
 
     next();
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,10 @@
 import { Router } from 'express';
 import notes from '../services/notes/routes/index.js';
 import users from '../services/users/routes/index.js';
-import authenticate from '../middlewares/auth.js';
 
 const router = Router();
 
 router.use('/', users);
-router.use(authenticate);
 router.use('/', notes);
 
 export default router;
