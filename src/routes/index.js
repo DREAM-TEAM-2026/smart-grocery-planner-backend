d88a3afe5fc4472d5997d6edee71b5fc30ee7026
Bagus Eka Pramudya <bepramudyaa@gmail.com>
2026-06-01 10:40:20 +0700

    chore: delete  notes route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,4 @@
 import { Router } from 'express';
-import notes from '../services/notes/routes/index.js';
 import users from '../services/users/routes/index.js';
 import calendar from '../services/calender/routes/index.js';
 import shoppingCart from '../services/shopping-cart/routes/index.js';
@@ -7,7 +6,6 @@ import shoppingCart from '../services/shopping-cart/routes/index.js';
 const router = Router();
 
 router.use('/', users);
-router.use('/', notes);
 router.use('/', calendar);
 router.use('/', shoppingCart);
 
